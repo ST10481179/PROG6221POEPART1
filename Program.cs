2e89515247d7795656bc12810c1eff5082d08316
Tankiso <tankiso@email.com>
2026-04-11 22:39:52 +0200

    Added Program.cs - main entry point with chatbot initialization

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CyberSecurityChatbot
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "CyberSecurity Awareness Bot";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            Chatbot.DisplayAsciiBanner();
+            AudioPlayer.PlayGreeting();
+
+            var user = new User();
+            var bot = new Chatbot(user);
+            bot.Run();
+        }
+    }
+}
+
